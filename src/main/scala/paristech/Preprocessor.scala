03bb9f7090bda310cc2d45d02b602bc526a6934e
magobol <jorge.antequera.magnani@gmail.com>
2019-10-30 18:30:24 +0100

    Revert "Preprocesser and trainer"
    This reverts commit 02344e1ab6fd71f81abc8c8103aa05b09838e3d9.

--- src/main/scala/paristech/Preprocessor.scala
+++ src/main/scala/paristech/Preprocessor.scala
@@ -1,13 +1,12 @@
 package paristech
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions._
-import org.apache.spark.SparkConf
-
+import org.apache.spark.sql.SparkSession
 
 object Preprocessor {
+
   def main(args: Array[String]): Unit = {
+
     // Des réglages optionnels du job spark. Les réglages par défaut fonctionnent très bien pour ce TP.
     // On vous donne un exemple de setting quand même
     val conf = new SparkConf().setAll(Map(
@@ -29,9 +28,6 @@ object Preprocessor {
       .appName("TP Spark : Preprocessor")
       .getOrCreate()
 
-    import spark.implicits._
-
-
     /*******************************************************************************
       *
       *       TP 2
@@ -44,48 +40,8 @@ object Preprocessor {
       *
       ********************************************************************************/
 
-    val df:DataFrame = spark
-      .read
-      .option("header", true) // utilise la première ligne du (des) fichier(s) comme header
-      .option("inferSchema", "true") // pour inférer le type de chaque colonne (Int, String, etc.)
-      .csv("/home/jorge/Documents/Cours/Spark/RepoAdotTPs/data/train_clean.csv")
-
-    println(s"Nombre de lignes : ${df.count}")
-    println(s"Nombre de colonnes : ${df.columns.length}")
-
-        val df3: DataFrame = df
-        .withColumn("goal", $"goal".cast("Int"))
-        .withColumn("deadline" , $"deadline".cast("Int"))
-        .withColumn("state_changed_at", $"state_changed_at".cast("Int"))
-        .withColumn("created_at", $"created_at".cast("Int"))
-        .withColumn("launched_at", $"launched_at".cast("Int"))
-        .withColumn("backers_count", $"backers_count".cast("Int"))
-        .withColumn("final_status", $"final_status".cast("Int"))
-        .dropDuplicates("deadline")
-        .filter(!isnull($"state_changed_at"))
-        .withColumn("country",when($"country" === "False",$"currency").otherwise($"country"))
-        .filter(($"disable_communication"==="True") || ($"disable_communication"==="False"))
-        .drop("disable_communication")
-        .filter($"country" rlike ".{2}")
-        .filter($"currency" rlike ".{3}")
-        .drop("backers_count","state_changed_at")
-        .withColumn("days_campaign",datediff(from_unixtime($"deadline"),from_unixtime($"launched_at")))
-        .withColumn("hours_prepa",(($"launched_at"-$"created_at")/60).cast("Int"))
-        .drop("launched_at","deadline","created_at")
-        .withColumn("name",lower($"name"))
-        .withColumn("desc",lower($"desc"))
-        .withColumn("keywords",lower($"keywords"))
-        .withColumn("text",concat($"name",lit(" "),$"desc",lit(" "),$"keywords"))
-        .withColumn("days_campaign",when(isnull($"days_campaign"),-1).otherwise($"days_campaign"))
-        .withColumn("hours_prepa",when(isnull($"hours_prepa"),-1).otherwise($"hours_prepa"))
-        .withColumn("goal",when(isnull($"goal"),-1).otherwise($"goal"))
-        .withColumn("country",when(isnull($"country")," ").otherwise($"country"))
-        .withColumn("currency",when(isnull($"currency")," ").otherwise($"currency"))
-
-
-//        df3.write.parquet("/home/jorge/Documents/Git/spark_project_kickstarter_2019_2020/cleanData.parquet")
-        df3.show(5)
+    println("\n")
+    println("Hello World ! from Preprocessor")
+    println("\n")
   }
-
-
 }
